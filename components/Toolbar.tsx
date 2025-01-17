535070ab6db084d270f16ff24f22359146d6fb8d
SoucaThomas <thomassouca@gmail.com>
2025-01-17 16:12:43 +0200

    refactor: update import paths, enhance theme support, and add toast functionality

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,10 +1,23 @@
-import { Trash2 } from "lucide-react";
-import { Check } from "lucide-react";
-import { RefObject } from "react";
+import { Trash2, Check, Palette, Brush, Eraser } from "lucide-react";
+import { RefObject, useEffect, useState } from "react";
 import { Swatch } from "../utils/Swatch";
 import axios from "axios";
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "@/hooks/useToast";
 import { motion } from "framer-motion";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const SCALE = 1.2;
 
@@ -28,6 +41,8 @@ export default function Toolbar(props: {
   } = props;
 
   const { toast } = useToast();
+  const [selectedId, setSelectedId] = useState(-1);
+  const [open, setOpen] = useState(false);
 
   const handleSubmint = async () => {
     setLoading(true);
@@ -42,7 +57,6 @@ export default function Toolbar(props: {
       .then((res) => res.data);
 
     if (response.error) {
-      // toast.error(response.message);
       toast({
         variant: "destructive",
         title: "Uh oh! Something went wrong.",
@@ -72,26 +86,31 @@ export default function Toolbar(props: {
     setLatex("");
   };
 
+  useEffect(() => {
+    console.log(selectedId);
+  }, [selectedId]);
+
   return (
     <motion.div
-      className="absolute inset-x-0 bottom-0 m-6 mx-auto flex h-16 max-w-2xl items-center justify-center gap-6 rounded-full bg-zinc-900"
+      className="absolute inset-x-0 bottom-0 m-6 mx-auto flex h-16 w-2/3 items-center justify-center gap-3 rounded-full bg-zinc-900 md:gap-6"
       initial={{ y: 100 }}
       animate={{ y: 0 }}
       transition={{ duration: 0.5 }}
     >
       <motion.div
-        className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg bg-red-500/90"
+        className="flex aspect-square h-8 cursor-pointer items-center justify-center rounded-lg bg-red-500/90 md:h-10"
         onClick={clearCanvas}
         whileHover={{ scale: SCALE }}
       >
         <Trash2 />
       </motion.div>
-      <div className="flex gap-4">
+
+      <div className="hidden gap-4 md:flex">
         {Object.values(Swatch).map((swatch) => (
           <motion.div
             className={`${
               currentSwatch === swatch ? "border-2" : ""
-            } h-6 w-6 rounded-lg`}
+            } h-4 w-4 cursor-pointer rounded-lg`}
             style={{ backgroundColor: swatch }}
             key={swatch}
             onClick={() => setCurrentSwatch(swatch)}
@@ -100,17 +119,69 @@ export default function Toolbar(props: {
         ))}
       </div>
 
+      <motion.div
+        className="flex aspect-square h-8 cursor-pointer items-center justify-center rounded-lg md:hidden md:h-10"
+        onClick={() => {
+          setSelectedId(0);
+        }}
+        whileHover={{ scale: SCALE }}
+      >
+        <DropdownMenu open={open} onOpenChange={setOpen}>
+          <DropdownMenuTrigger asChild>
+            <div onClick={() => setSelectedId(0)}>
+              <Palette />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center" className="w-[2rem]">
+            <DropdownMenuLabel>Colors</DropdownMenuLabel>
+            <DropdownMenuGroup>
+              {Object.values(Swatch).map((swatch) => (
+                <DropdownMenuItem key={swatch}>
+                  <button
+                    className={`${
+                      currentSwatch === swatch ? "border-2" : ""
+                    } h-5 w-10 cursor-pointer rounded-lg`}
+                    style={{ backgroundColor: swatch }}
+                    key={swatch}
+                    onClick={() => setCurrentSwatch(swatch)}
+                  ></button>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </motion.div>
+
+      <motion.div
+        className="flex aspect-square h-10 w-10 cursor-pointer items-center justify-center rounded-lg md:hidden"
+        onClick={() => {
+          setSelectedId(1);
+        }}
+        whileHover={{ scale: SCALE }}
+      >
+        <Brush />
+      </motion.div>
       <input
         type="range"
         min={1}
         max={40}
         value={currentSize}
         onChange={(event) => setCurrentSize(Number(event.target.value))}
-        className="color-zinc-200 accent-white"
+        className="color-zinc-200 hidden accent-white md:block"
       />
 
       <motion.div
-        className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg bg-green-500/90"
+        className="flex aspect-square h-10 w-10 cursor-pointer items-center justify-center rounded-lg"
+        onClick={() => {
+          setSelectedId(2);
+        }}
+        whileHover={{ scale: SCALE }}
+      >
+        <Eraser />
+      </motion.div>
+
+      <motion.div
+        className="flex aspect-square h-8 cursor-pointer items-center justify-center rounded-lg bg-green-500/90 md:h-10"
         onClick={handleSubmint}
         whileHover={{ scale: SCALE }}
       >
