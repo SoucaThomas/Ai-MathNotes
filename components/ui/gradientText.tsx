00d4239ccbbe065c1e63b23ed92b27d0ab47f812
SoucaThomas <thomassouca@gmail.com>
2025-01-17 18:51:58 +0200

    run prettier

--- components/ui/gradientText.tsx
+++ components/ui/gradientText.tsx
@@ -28,7 +28,7 @@ export default function GradientText({
     >
       {showBorder && (
         <div
-          className="animate-gradient pointer-events-none absolute inset-0 z-0 bg-cover"
+          className="pointer-events-none absolute inset-0 z-0 animate-gradient bg-cover"
           style={{
             ...gradientStyle,
             backgroundSize: "300% 100%",
@@ -47,7 +47,7 @@ export default function GradientText({
         </div>
       )}
       <div
-        className="z-2 animate-gradient relative inline-block bg-cover text-transparent"
+        className="z-2 relative inline-block animate-gradient bg-cover text-transparent"
         style={{
           ...gradientStyle,
           backgroundClip: "text",
